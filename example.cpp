40e0b6934d709035f0a5b21299d1dfccd5b849f7
Radeel Ayesha <radeelayesha03@gmail.com>
2024-04-30 00:47:18 -0700

    added a line of code

--- example.cpp
+++ example.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
- int a;
-cout<<"Enter the value of a:";
-cin>>a;
+      int a;
+      cout<<"Enter the value of a:";
+      cin>>a;
+int b;
+cout<<"Enter value of b:";
+cin>>b;
+
+int sum=0;
+sum=a+b;
+cout<<sum;
+
 
 
 return 0;
